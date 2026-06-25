426beb69982887dc333a6f7a957cedcfdac6887b
bahaztariq <bahaztariq@gmail.com>
2026-06-25 09:24:09 +0100

    chore: configure axios default base url and interceptors

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,15 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.interceptors.request.use((config) => {
+    const token = Cookies.get('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
