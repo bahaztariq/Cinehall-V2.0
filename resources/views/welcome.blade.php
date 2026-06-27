9d1f6a82ed29d8154850f0d21260eac48ed1ddae
bahaztariq <bahaztariq@gmail.com>
2026-06-27 15:37:05 +0100

    style: link welcome layout icon to the new logo asset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/png" href="/assets/logo_icon.png">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="antialiased">
